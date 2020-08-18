da25fdad819c8b24ad7daadd6a52251b4caa8049
varadha17 <abinash.varadharajan@gmail.com>
2020-08-18 21:59:58 +0530

    Write excel1

--- src/main/java/com/Base/BaseTest.java
+++ src/main/java/com/Base/BaseTest.java
@@ -7,7 +7,6 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
--- src/main/java/com/Pages/frontPage.java
+++ src/main/java/com/Pages/frontPage.java
@@ -2,7 +2,6 @@ package com.Pages;
 
 import java.io.IOException;
 
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
